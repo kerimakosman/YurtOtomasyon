a93e855b52d975d2b388b94a5b4ee05533f2f410
kerimakosman <111852730+kerimakosman@users.noreply.github.com>
2022-12-08 19:09:07 +0300

    OdaCreate

--- Yurt.BL/ViewModels/OdaVM/OdaCreateVM.cs
+++ Yurt.BL/ViewModels/OdaVM/OdaCreateVM.cs
@@ -1,8 +1,13 @@
-﻿namespace Yurt.BL.ViewModels.OdaVM
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Yurt.BL.ViewModels.OdaVM
 {
     public class OdaCreateVM
     {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Oda No zorunlu alan")]
         public string OdaNo { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Kapasite zorunlu alan")]
         public byte Kapasite { get; set; }
     }
 }
